b8418eda9df955cc20513be8d544a3885db48161
dno01 <dno01>
2026-05-04 10:45:28 +0200

    funktioner

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
 console.log("log")
+
+// denna ska skicka en ny log till google spreadsheets
+const postLog = () => {
+    
+}
+
+// denna ska hämta samtliga loggar till google spreadsheets
+const getLogs = () => {
+
+}
+
+// denna ska radera en log från google spreadsheets
+const deleteLog = (id) => {
+
+}
